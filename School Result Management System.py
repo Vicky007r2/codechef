3a0127a45b839d9cf7a6a58a306b513ecd6158fb
Vicky007r2 <vigneshkumar20055@gmail.com>
2026-06-02 15:27:05 +0530

    Create School Result Management System.py

---
+++ School Result Management System.py
@@ -0,0 +1,17 @@
+n=input()
+a,b,c,d,e=map(int,input().split())
+total=a+b+c+d+e
+av=(a+b+c+d+e)/5
+m=max(a,b,c,d,e)
+if a<35:
+  print("fail")
+elif b<35:
+  print("fail") 
+elif c<35:
+  print("fail")
+elif d<35:
+  print("fail") 
+elif f<35:
+  print("fail")
+else:        
+  print("pass")
