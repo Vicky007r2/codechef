ebeb2101f3a1a0575cef7df613b80ade24edf991
Vicky007r2 <vigneshkumar20055@gmail.com>
2026-06-02 15:33:20 +0530

    Create University Attendance Management System.py

---
+++ University Attendance Management System.py
@@ -0,0 +1,12 @@
+n=input()
+a,b,c,d,e,k=map(int,input().split())
+av=(a+b+c+d+e+k)//6
+count=0
+m=max(a,b,c,d,e,k)
+mi=min(a,b,c,d,e,k)
+for i in a,b,c,d,e,k:
+  if i>=75:
+    count+=1
+print(m)
+print(mi)    
+print(count)  
